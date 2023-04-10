70a0763fa26c1710cd867fb03d5df6f6d2b452a0
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-04-10 22:33:16 +0530

    ADD: image slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,6 +54,7 @@ const Nav = styled.nav`
     display: flex;
     align-items: center;
     padding: 0 36px;
+    overflow-x: hidden;
 `
 const Logo = styled.img`
     width: 80px;
@@ -86,6 +87,7 @@ const NavMenu = styled.div`
           bottom: -6px;
           opacity: 0;
           transform: scaleX(0);
+          transform-origin: left center;
           transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
         }
       }
---
+++ src/components/Viewers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Viewers() {
+  return (
+    <div>Viewers</div>
+  )
+}
+
+export default Viewers
