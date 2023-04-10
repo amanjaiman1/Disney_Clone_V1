061ee50dfa46dee5846be588ed14d7eeb1b25b0b
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-04-10 21:26:56 +0530

    Add: header

---
+++ src/components/Header.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header () {
+  return (
+    <Nav>
+        <Logo src="/images/logo.svg" />
+        <NavMenu
+          className="ml-6 items-center"
+        >
+          <a>
+            <img src="/images/home-icon.svg" />
+            <span>HOME</span>
+          </a>
+
+          <a>
+            <img src="/images/search-icon.svg" />
+            <span>SEARCH</span>
+          </a>
+
+          <a>
+            <img src="/images/watchlist-icon.svg" />
+            <span>WATCHLIST</span>
+          </a>
+
+          <a>
+            <img src="/images/original-icon.svg" />
+            <span>ORIGINALS</span>
+          </a>
+
+          <a>
+            <img src="/images/movie-icon.svg" />
+            <span>MOVIES</span>
+          </a>
+
+          <a>
+            <img src="/images/series-icon.svg" />
+            <span>SERIES</span>
+          </a>
+
+        </NavMenu>
+        <UserImg src="" />
+        
+    </Nav>
+  )
+}
+
+export default Header
+
+
+const Nav = styled.nav`
+    height: 70px;
+    background: #090b13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
+`
+const Logo = styled.img`
+    width: 80px;
+`
+
+const NavMenu = styled.div`
+    display: flex;
+    
+    a{
+      display: flex;
+      align-items: center;
+      padding: 0 12px;
+
+      img{
+        height: 20px;
+      }
+
+      span{
+        font-size: 13px;
+        letter-spacing: 1.42px;
+        position: relative;
+
+        &:after{
+          content: "";
+          position: absolute;
+          height: 2px;
+          background: white;
+          left: 0;
+          right: 0;
+          bottom: -6px;
+          opacity: 0;
+          transform: scaleX(0);
+          transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        }
+      }
+
+      &:hover{
+        span:after{
+          transform: scaleX(1);
+          opacity: 1;
+
+        }
+      }
+    }
+`
+
+const UserImg = styled.img`
+
+
+`
